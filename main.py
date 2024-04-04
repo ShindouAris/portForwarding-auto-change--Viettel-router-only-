d87aa1ab6789b8674dafbce81bef2004af36f2d8
CHISADIN <tranthanhnam.ccc@gmail.com>
2024-04-04 20:53:55 +0700

    Tự chọn chế độ tự động để lấy ip sau 5s

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from os import system
 from utils.getipv4 import getipv4
 from utils.run import *
+import asyncio
+from inputimeout import inputimeout
 
 def check(v4ip):
     if v4ip == "192.168.1.1":
@@ -13,46 +15,55 @@ def check(v4ip):
             "status": "OK",
             "msg": "IP hợp lệ"
         }
-    
+timeout = 5
 
 
 try: system("cls")
 except: system("clear")
+async def main():
+        global select, set_v4ip
+        try:
+            select = inputimeout(f"Vui lòng chọn 2 chế độ sau đây: \n [1]: Tự động lấy ip \n [2]: Nhập ip thủ công\n Lưu ý: Sẽ tự động chọn ip nếu người dùng không tương tác sau {timeout} giây \nLựa chọn của bạn: ", timeout=timeout)
+        except Exception:
+            set_v4ip = getipv4()
+        try:
+            if int(select) ==  1:
+                set_v4ip = getipv4()
+            elif int(select) > 2:
+                print("Lựa chon không hợp lệ!")
+                system("shutdown -r -t 10")
+                set_v4ip = "192.168.1.1"
+            else:
+                try:
+                    try: system("cls")
+                    except: system("clear")
+                    ip = input("Nhập IPV4 của bạn: ")
+                except KeyboardInterrupt:
+                    exit(0)
 
-select = input("Vui lòng chọn 2 chế độ sau đây: \n [1]: Tự động lấy ip \n [2]: Nhập ip thủ công\n Lựa chọn của bạn: ")
+            set_v4ip = f"192.168.1.{ip}"
+        except NameError:
+            pass
 
-if int(select) ==  1:
-    set_v4ip = getipv4()
-elif int(select) > 2:
-    print("Lựa chon không hợp lệ!")
-    system("shutdown -r -t 10")
-    set_v4ip = "192.168.1.1"
-else:
-    try:
-        try: system("cls")
-        except: system("clear")
-        ip = input("Nhập IPV4 của bạn: ")
-    except KeyboardInterrupt:
-        exit(0)
+        res = check(set_v4ip)
 
-    set_v4ip = f"192.168.1.{ip}"
+        if res["status"] == "OK":
+            try: system("cls")
+            except: system("clear")
+            print(f"Đang tiến hành đổi cổng: {set_v4ip}")
+            program = portForward(set_v4ip)
+            if program["status"] == "Done":  
+                system("cls")
+                print(f"Đã đổi cổng chuyển tiếp sang ipv4: {set_v4ip}\n")
+                time.sleep(10)
+                system("pause")
+            elif program["status"] == "failed":
+                print(f"Đã xảy ra lỗi: {repr(program['msg'])}")
+                
+        else:
+            print(res["msg"])
+            system("pause")
 
-res = check(set_v4ip)
-
-if res["status"] == "OK":
-    try: system("cls")
-    except: system("clear")
-    print(f"Đang tiến hành đổi cổng: {set_v4ip}")
-    program = portForward(set_v4ip)
-    if program["status"] == "Done":  
-        system("cls")
-        print(f"Đã đổi cổng chuyển tiếp sang ipv4: {set_v4ip}\n")
-        time.sleep(10)
-        system("pause")
-    elif program["status"] == "failed":
-        print(f"Đã xảy ra lỗi: {repr(program["msg"])}")
-        
-else:
-    print(res["msg"])
-    system("pause")
+if __name__ == "__main__":
+    asyncio.run(main())
 
